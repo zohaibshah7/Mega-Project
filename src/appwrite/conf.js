c5e246f30495a75b2799faa50b39103282ceb474
Syed Zohaib Shah <syedzohaibshah969@gmail.com>
2024-12-11 14:55:54 +0500

    setup Queries in All posts

--- src/appwrite/conf.js
+++ src/appwrite/conf.js
@@ -77,6 +77,19 @@ export class Service {
       return false;
     }
   }
+
+  async getPosts(queries = [Query.equal("status", "active")]) {
+    try {
+      return await this.databases.listDocuments(
+        config.appwriteDatabaseId,
+        config.appwriteCollectionId,
+        queries
+      );
+    } catch (error) {
+      console.log("Appwrite service :: getPosts :: error", error);
+      return false;
+    }
+  }
 }
 
 const service = new Service();
