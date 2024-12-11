5835520e173d3fe5f07cb891f0d043fece395a7e
Syed Zohaib Shah <syedzohaibshah969@gmail.com>
2024-12-11 18:11:48 +0500

    Styling Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
-import "./App.css";
 import authService from "./appwrite/auth";
 import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
+import "./App.css";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -22,10 +23,13 @@ function App() {
   }, []);
 
   return !loading ? (
-    <div>
-      
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <Footer />
+      </div>
     </div>
-  ) : null
+  ) : null;
 }
 
 export default App;
