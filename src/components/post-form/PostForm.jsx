521aeefe8a7d57214a66acd6eb0769956c5f2aa6
Syed Zohaib Shah <syedzohaibshah969@gmail.com>
2024-12-16 13:30:13 +0500

    Upload file process logic

---
+++ src/components/post-form/PostForm.jsx
@@ -0,0 +1,33 @@
+import React, { useCallback } from "react";
+import { useForm } from "react-hook-form";
+import { Button, Input, Select, RTE } from "../index";
+import appwriteService from "../../appwrite/conf";
+import { data, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+function PostForm({ post }) {
+  const { register, handleSubmit, watch, setValue, control, getValues } =
+    useForm({
+      defaultValues: {
+        title: post?.title || "",
+        slug: post?.slug || "",
+        content: post?.content || "",
+        status: post?.status || "active",
+      },
+    });
+
+  const navigate = useNavigate();
+  const userData = useSelector((state) => state.user.userData);
+
+  const submit = async (data) => {
+    if (post) {
+      const file = data.image[0]
+        ? appwriteService.uploadFile(data.image[0])
+        : null;
+    }
+  };
+
+  return <div></div>;
+}
+
+export default PostForm;
