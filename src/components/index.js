decbf0f76298b7c76d8de8ee64936052742b2e2c
Syed Zohaib Shah <syedzohaibshah969@gmail.com>
2024-12-16 16:51:12 +0500

    Building Pages

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,21 @@ import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
 import Signup from "./Signup";
+import Login from "./Login";
+import Post from "./Post";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
 
-export { Header, Footer, Container, Logo, LogoutBtn, RTE, Signup };
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  RTE,
+  Signup,
+  Login,
+  Post,
+  PostForm,
+  PostCard,
+};
