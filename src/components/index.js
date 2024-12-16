237d334a21055af282c5bc8233e1dcf6d338f6b8
Syed Zohaib Shah <syedzohaibshah969@gmail.com>
2024-12-16 18:16:27 +0500

    Debugging import export

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,17 @@
+import Select from "./Select";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from "./Container/Container";
+import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
 import Signup from "./Signup";
 import Login from "./Login";
+import Button from "./Button";
 import PostForm from "./post-form/PostForm";
 import PostCard from "./PostCard";
-import AuthLayout from "./AuthLayout"
+import AuthLayout from "./AuthLayout";
+import Input from "./Input";
 
 export {
   Header,
@@ -19,7 +22,10 @@ export {
   RTE,
   Signup,
   Login,
+  Button,
   PostForm,
   PostCard,
-  AuthLayout
+  AuthLayout,
+  Input,
+  Select,
 };
