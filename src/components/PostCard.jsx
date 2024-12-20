899e5218f9b4a93453fab1f0abaef414f5ad93d1
Syed Zohaib Shah <syedzohaibshah969@gmail.com>
2024-12-20 19:54:13 +0500

    flexing the post cards

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -5,8 +5,8 @@ import { Link } from "react-router-dom";
 function PostCard({ $id, title, featuredImage }) {
   return (
     <Link to={`/post/${$id}`}>
-      <div className="w-full bg-gray-100 rounded-xl p-4">
-        <div className="w-full justify-center mb-4">
+      <div className="w-full hover:shadow-xl bg-gray-100 rounded-xl p-4">
+        <div className="w-full flex items-center justify-center mb-4">
           <img
             src={appwriteService.getFilePreview(featuredImage)}
             alt={title}
